1d6132463924288ae731fee18c85988f033f3e74
Adrien Guatto <adrien.guatto@laposte.net>
2009-06-01 02:50:42 +0200

    Corrected dumb bugs due to project switching.

--- example.ml
+++ example.ml
@@ -13,37 +13,37 @@ struct
 
   type pattern_ast =
     | True | False
-    | OrA of pattern_ast * pattern_ast
+    | Or of pattern_ast * pattern_ast
     | Left of pattern_ast | Right of pattern_ast
     | Couple of pattern_ast * pattern_ast
     | First of pattern_ast | Second of pattern_ast
     | Var of string
 
   let rec inject c = match c with
-    | True -> Constr ("True", []) | False -> Constr ("False", [])
-    | OrA (l, r) -> Or (inject l, inject r)
-    | Left p -> Constr ("Left", [inject p])
-    | Right p -> Constr ("Right", [inject p])
-    | First p -> Constr ("First", [inject p])
-    | Second p -> Constr ("Second", [inject p])
-    | Couple (l, r) -> Constr ("Couple", [inject l; inject r])
-    | Var _ -> Any
+    | True -> Pconstr ("True", []) | False -> Pconstr ("False", [])
+    | Or (l, r) -> Por (inject l, inject r)
+    | Left p -> Pconstr ("Left", [inject p])
+    | Right p -> Pconstr ("Right", [inject p])
+    | First p -> Pconstr ("First", [inject p])
+    | Second p -> Pconstr ("Second", [inject p])
+    | Couple (l, r) -> Pconstr ("Couple", [inject l; inject r])
+    | Var _ -> Pany
 
   let rec eject c = match c with
-    | Or (l, r) -> OrA (eject l, eject r)
-    | Constr ("True", []) -> True | Constr ("False", []) -> False
-    | Constr ("Left", [p]) -> Left (eject p)
-    | Constr ("Right", [p]) -> Right (eject p)
-    | Constr ("First", [p]) -> First (eject p)
-    | Constr ("Second", [p]) -> Second (eject p)
-    | Constr ("Couple", [cl; cr]) -> Couple (eject cl, eject cr)
-    | Any -> Var "_"
+    | Por (l, r) -> Or (eject l, eject r)
+    | Pconstr ("True", []) -> True | Pconstr ("False", []) -> False
+    | Pconstr ("Left", [p]) -> Left (eject p)
+    | Pconstr ("Right", [p]) -> Right (eject p)
+    | Pconstr ("First", [p]) -> First (eject p)
+    | Pconstr ("Second", [p]) -> Second (eject p)
+    | Pconstr ("Couple", [cl; cr]) -> Couple (eject cl, eject cr)
+    | Pany -> Var "_"
     | _ -> invalid_arg "TEST_SIG.eject"
 
   let rec pp_pattern_ast p = match p with
     | True -> "True"
     | False -> "False"
-    | OrA (l, r) -> pp_pattern_ast l ^ "|" ^ pp_pattern_ast r
+    | Or (l, r) -> pp_pattern_ast l ^ "|" ^ pp_pattern_ast r
     | Left p' -> Printf.sprintf "Left %s\n" (pp_pattern_ast p')
     | Right p' -> Printf.sprintf "Right %s\n" (pp_pattern_ast p')
     | First p' -> Printf.sprintf "(fst %s)\n" (pp_pattern_ast p')
@@ -87,7 +87,7 @@ open TEST_CHECKER
 let _ =
   let p =
     [(Couple (True, Var "x"));
-     (OrA (Couple (False, True), Couple(False, True)));
+     (Or (Couple (False, True), Couple(False, True)));
      (Couple (True, False))] in
   let r = check p in
   begin match r.not_matched with
